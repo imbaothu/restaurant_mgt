3c879b28761d6a88fa77892670f9b267efaf0d78
Thu Le <tbl005@bucknell.edu>
2025-03-12 13:09:39 -0400

    Initial commit - Restaurant API setup

---
+++ src/main/java/org/example/restaurant_mgt/RestaurantMgtApplication.java
@@ -0,0 +1,13 @@
+package org.example.restaurant_mgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantMgtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantMgtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/restaurant_mgt/RestaurantMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restaurant_mgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantMgtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
